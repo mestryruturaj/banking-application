700e6573228a4de121ddf77857d4d19ba5f3a683
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-12 18:19:47 +0530

    added common place to add exception and response messages

---
+++ src/main/java/io/enscryptingbytes/banking_application/message/ExceptionMessage.java
@@ -0,0 +1,5 @@
+package io.enscryptingbytes.banking_application.message;
+
+public class ExceptionMessage {
+    public static final String USER_ALREADY_EXISTS = "User already exist.";
+}
---
+++ src/main/java/io/enscryptingbytes/banking_application/message/ResponseMessage.java
@@ -0,0 +1,10 @@
+package io.enscryptingbytes.banking_application.message;
+
+public class ResponseMessage {
+    public static final String USER_CREATION_PASSED = "User creation completed.";
+    public static final String USER_CREATION_FAILED = "User creation failed.";
+    public static final String USER_FOUND = "User found.";
+    public static final String USER_DOES_NOT_EXIST = "User does not exist.";
+    public static final String SUCCESS = "Success";
+    public static final String FAILED = "Failed";
+}
