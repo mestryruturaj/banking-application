28ad68694453920df0492f6c83982b8fa81a192e
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-12 20:50:47 +0530

    added exception messages

--- src/main/java/io/enscryptingbytes/banking_application/message/ExceptionMessage.java
+++ src/main/java/io/enscryptingbytes/banking_application/message/ExceptionMessage.java
@@ -2,4 +2,5 @@ package io.enscryptingbytes.banking_application.message;
 
 public class ExceptionMessage {
     public static final String USER_ALREADY_EXISTS = "User already exist.";
+    public static final String USER_DOES_NOT_EXISTS = "User does not exist.";
 }
