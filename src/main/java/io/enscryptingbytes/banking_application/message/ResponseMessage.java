28c5e75dcf72e7e02f54d498a21aec8cd78967be
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-15 11:11:54 +0530

    added new response messages

--- src/main/java/io/enscryptingbytes/banking_application/message/ResponseMessage.java
+++ src/main/java/io/enscryptingbytes/banking_application/message/ResponseMessage.java
@@ -3,8 +3,10 @@ package io.enscryptingbytes.banking_application.message;
 public class ResponseMessage {
     public static final String USER_CREATION_PASSED = "User creation completed.";
     public static final String USER_CREATION_FAILED = "User creation failed.";
+    public static final String USER_WITH_MOBILE_NUMBER_EXIST = "User with entered mobile number already exists.";
     public static final String USER_FOUND = "User found.";
     public static final String USER_DOES_NOT_EXIST = "User does not exist.";
     public static final String SUCCESS = "Success";
     public static final String FAILED = "Failed";
+    public static final String SOMETHING_WENT_WRONG = "Something went wrong.";
 }
