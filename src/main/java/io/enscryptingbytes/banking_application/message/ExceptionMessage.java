3a22a0448d8e3271b53803d8a47e6b9c25c6dd60
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-16 19:31:34 +0530

    Created custom branch exception and exception messages

--- src/main/java/io/enscryptingbytes/banking_application/message/ExceptionMessage.java
+++ src/main/java/io/enscryptingbytes/banking_application/message/ExceptionMessage.java
@@ -3,4 +3,7 @@ package io.enscryptingbytes.banking_application.message;
 public class ExceptionMessage {
     public static final String USER_ALREADY_EXISTS = "User already exist.";
     public static final String USER_DOES_NOT_EXISTS = "User does not exist.";
+
+    public static final String BRANCH_ALREADY_EXISTS = "Branch already exist.";
+    public static final String BRANCH_DOES_NOT_EXISTS = "Branch does not exist.";
 }
