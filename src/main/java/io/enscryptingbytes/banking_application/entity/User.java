cdb10813faeeed45a63369bd21b59ff2f605e711
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-21 20:19:26 +0530

    Modified User entity to have a password. Added BankUserDetails which implements UserDetails.

--- src/main/java/io/enscryptingbytes/banking_application/entity/User.java
+++ src/main/java/io/enscryptingbytes/banking_application/entity/User.java
@@ -1,9 +1,6 @@
 package io.enscryptingbytes.banking_application.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -21,4 +18,6 @@ public class User extends AuditableEntity {
     private String email;
     private String mobileNumber;
     private String dob;
+    @Column(nullable = false)
+    private String password;
 }
---
+++ src/main/java/io/enscryptingbytes/banking_application/security/BankUserDetails.java
@@ -0,0 +1,31 @@
+package io.enscryptingbytes.banking_application.security;
+
+import io.enscryptingbytes.banking_application.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class BankUserDetails implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getEmail();
+    }
+}
