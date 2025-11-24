e325d49f5e95cad541039af36c1b7643b66f7fe8
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-24 19:39:21 +0530

    Added exception

--- src/main/java/io/enscryptingbytes/banking_application/message/ExceptionMessage.java
+++ src/main/java/io/enscryptingbytes/banking_application/message/ExceptionMessage.java
@@ -4,7 +4,7 @@ public class ExceptionMessage {
     public static final String INVALID_INPUT = "Invalid input.";
 
     public static final String USER_ALREADY_EXISTS = "User already exist.";
-    public static final String USER_DOES_NOT_EXISTS = "User does not exist.";
+    public static final String USER_DOES_NOT_EXIST = "User does not exist.";
 
     public static final String BRANCH_ALREADY_EXISTS = "Branch already exist.";
     public static final String BRANCH_DOES_NOT_EXISTS = "Branch does not exist.";
@@ -13,4 +13,6 @@ public class ExceptionMessage {
     public static final String ACCOUNT_DOES_NOT_EXISTS = "Account does not exist.";
     public static final String ACCOUNT_CREATION_FAILED_DUE_TO_INVALID_USER = "Account creation failed due to invalid user. Either input valid user or consider creating one.";
     public static final String ACCOUNT_CREATION_FAILED_DUE_TO_INVALID_BRANCH = "Account creation failed due to invalid branch.";
+
+    public static final String INCORRECT_CREDENTIALS = "Incorrect login or password.";
 }
---
+++ src/main/java/io/enscryptingbytes/banking_application/security/message/ExceptionMessage.java
@@ -0,0 +1,10 @@
+package io.enscryptingbytes.banking_application.security.message;
+
+public class ExceptionMessage {
+    public static final String USER_ALREADY_EXISTS = "User already exist.";
+    public static final String USER_DOES_NOT_EXIST = "User does not exist.";
+    public static final String INCORRECT_CREDENTIALS = "Incorrect login or password.";
+    public static final String NEW_PASSWORD_MISMATCH = "New password mismatch.";
+    public static final String OLD_PASSWORD_REQUIRED = "Old password required.";
+    public static final String TOKEN_REQUIRED = "Token required.";
+}
